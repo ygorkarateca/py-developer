70397b72041130c93ece06937c70c5976347762f
Ygor Fernandes <ygorkarateca@gmail.com>
2024-04-11 10:52:15 -0300

    mod-up

---
+++ 01 - Estruturas de dados/01 - Listas/00_teste.py
@@ -0,0 +1,11 @@
+# pares.append(numeros)
+
+convidados_parcial = ["Ygor", "Daniel", "Miguel", "Paulinho"]
+convidados_futuros = []
+
+opcao = int(input("Digite:\n[1]ADICIONAR\n[2]REMOVER\n[3]SAIR"))
+
+adicionar_convidados = str(input())
+
+if opcao == "1":
+    print()
