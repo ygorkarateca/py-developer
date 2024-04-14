924c077968f2f9fe3612db6c8a6962889972c5e7
Ygor Fernandes <115374517+ygorkarateca@users.noreply.github.com>
2024-04-14 07:53:01 -0300

    .

--- 01 - Estruturas de dados/01 - Listas/00_teste.py
+++ 01 - Estruturas de dados/01 - Listas/00_teste.py
@@ -1,19 +1,26 @@
 # pares.append(numeros)
 
-convidados_parcial = ["Ygor", "Daniel", "Miguel", "Paulinho"]
-convidados_futuros = []
+convidados = ["Ygor", "Daniel", "Miguel", "Paulinho"]
+convidados_futuros = ""
 
-opcao = int(input("Digite:\n[1]ADICIONAR\n[2]REMOVER\n[3]SAIR"))
-
-adicionar_convidados = str(input()).title()
+opcao = int(input("Digite:\n[1]ADICIONAR\n[2]REMOVER\n[3]SAIR\n"))
 
 while True:
 
+    # Adicionar nomes
     if opcao == "1":
-        print()
 
+        adicionar_convidados = str(input()).title()
+        print(f"Digite o nome para adicionar:{adicionar_convidados.append()}")
+
+    # Remover nomes
     elif opcao == "2":
-        pass
+        convidados.remove()
 
-else:
-    break
+    # Mostrar nomes atuais
+    elif opcao == "3":
+        print(convidados)
+
+    # Sair
+    else:
+        break
